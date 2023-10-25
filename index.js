4f855e1374406edee9b4cae78266b75bd126b0fb
CECIL7845 <odonkorcecil123@gmail.com>
2023-10-25 07:49:47 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"]
+let appleShelf = document.getElementById("apple-shelf")
+let orangeShelf = document.getElementById("orange-shelf")
+
+function sortfruit() {
+    for (let i = 0; i < fruit.length; i++)
+    {
+        if (fruit[i] === "🍎") 
+        {
+        appleShelf.textContent += "🍎"
+        }
+        else if (fruit[i] === "🍊") 
+        {
+        orangeShelf.textContent += "🍊"
+        }
+    }
+}
+
+sortfruit()
